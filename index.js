570929c7942f9c08c77761f81d7d1794c6178742
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-06-26 06:26:32 -0500

    inicializacion de server de socket

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 var app = require('./app');
 
-app.listen(4444, function () {
-    console.log('Servidor iniciado Http://localhost:'+4444);
-})
+app.listen(8080, function() {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
