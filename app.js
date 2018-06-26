2b0d6468cadfc3222302a3961d997479ea72bba0
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-06-26 09:10:20 -0500

    eventos de socket

--- app.js
+++ app.js
@@ -3,18 +3,59 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var celulares = [];
 
 
 io.on('connection', function(socket) {
   console.log('Alguien se ha conectado con Sockets');
-  socket.emit('messages', messages);
+  //socket.emit('messages', messages);
 
   socket.on('autenticated', function(data) {
-    messages.push(data);
+    console.log('  llegada ', socket.id);
+    if (validarExistencia(data.identificador)){
+        updateIdConneccion(data.identificador, socket.id);
+    }else{
+        data["conexion"]=socket.id;
+        celulares.push(data);
+    }
+    console.log(celulares);
+    io.to(socket.id).emit('response-autenticacion', {'llego': 'jajajajajaja'});
+  });
+  
+  
+  socket.on('solicitar-informacion', function (data) {
+    console.log('se deben de identificar ---> ',data);
+      console.log('celulares ---> ',celulares);
+      socket.broadcast.emit({'estado_servicio':true});
+      for(var i=0; i < celulares.length; i++){
+        io.to(celulares[i].identificador).emit({'estado_servicio':true});
+      }
+      console.log('no lo eealizo');
+  });
 
-    io.sockets.emit('messages', messages);
+  socket.on('identificarme', function (data) {
+      console.log(data);
   });
 });
+
+
+function validarExistencia(identificador){
+  for (var i=0; i < celulares.length; i++){
+    if(identificador == celulares[i].identificador){
+      return true;
+    }
+  }
+  return false;
+}
+
+function updateIdConneccion(identificador, value){
+  for (var i=0; i < celulares.length; i++){
+    if(identificador == celulares[i].identificador){
+      celulares[i].conexion=value;
+    }
+  }
+
+
+}
 
 module.exports = server;
