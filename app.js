94e5cfa6aabc2ae05760b7ffd9339f8d10b5fbdb
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-06-26 06:27:07 -0500

    creacion del server

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
 'use strict'
-
 var express = require('express');
-var app = require('http').createServer(express);
-var io = require('socket.io').listen(app);
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 
-io.configure('development', function(){
-  io.set('transports', ['websocket']);
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('autenticated', function(data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
 });
 
-io.sockets.on('connection', function(client){
-    var userName;
-	console.log("user connected!");
-	client.emit('message', 'please insert user name');
-
-	client.on('message', function(message){
-        if (!userName) {
-			userName = message;
-			console.log(userName + ' is connected :)');
-			client.emit('message', 'Welcome ' + userName);
-			client.broadcast.emit('message', userName + ' is connected');
-		}
-		else {
-			client.emit('message', 'me: ' + message);
-			client.broadcast.emit('message', userName + ' says: ' + message);
-		}
-    });
-
-    client.on('disconnect', function() {
-		if (userName) {
-			console.log(userName + " left");
-			client.broadcast.emit('message', userName + ' left us :(');
-		}
-		else {
-			console.log("anonymous left");
-		}
-    });
-});
-
-module.exports = app;
+module.exports = server;
